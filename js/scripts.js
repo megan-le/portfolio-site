6e94ecd68b978026621166be43053f6f14ce1897
megan-le <134005189+megan-le@users.noreply.github.com>
2023-07-11 09:49:29 -0500

    Usability Test & Validator Edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,35 @@
-console.log("Hi, welcome to my portfolio site!")
+console.log("Hi, welcome to my portfolio site!");
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-    } else {
-        x.className = 'navtoggle';
-    }
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
+
+// Get the button:
+let fabbutton = document.getElementById("fabbutton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 750 ||
+    document.documentElement.scrollTop > 750
+  ) {
+    fabbutton.style.display = "block";
+  } else {
+    fabbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
