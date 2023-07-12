20e22b490e5a9454156704d024d704a879043be9
megan-le <134005189+megan-le@users.noreply.github.com>
2023-07-12 17:13:52 -0500

    MoneyMerge Page Edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 console.log("Hi, welcome to my portfolio site!");
 
 function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
-    x.className += " responsive";
+  var x = document.getElementById('myNavtoggle');
+	var isMobileBreakpoint = window.innerWidth < 641
+
+  if (x.className === 'navtoggle' && isMobileBreakpoint) {
+    x.className += ' responsive';
   } else {
-    x.className = "navtoggle";
+    x.className = 'navtoggle';
   }
 }
 
